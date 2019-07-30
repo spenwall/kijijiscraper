49677c473181eabcbb78d2569d6c3fdf37aee81d
Spencer Wallace <dude.wallace@gmail.com>
2019-07-29 23:23:38 -0600

    Body returns titles for all first page ads

--- KijijiScrape/components/scraper.js
+++ KijijiScrape/components/scraper.js
@@ -2,7 +2,7 @@ const cheerio = require("cheerio");
 const request = require("request-promise");
 
 module.exports = async req => {
-  const url = "https://www.kijiji.ca/b-bikes/medicine-hat/c644l1700231";
+  const url = "https://www.kijiji.ca/" + req.query.category + "/" + req.query.location + "/" + req.query.code;
   let options = {
     uri: url,
     transform: function(body) {
@@ -11,11 +11,19 @@ module.exports = async req => {
   };
 
   const $ = await request(options);
-  let ads = $(".regular-ad");
-  let titles = [];
-  ads.each((i, ad) => {
-    titles[i] = ad.find('.title');
+  let regularAds = $(".regular-ad");
+  let ads = [];
+  regularAds.each((i, ad) => {
+    ads[i] = {
+      title: $(ad)
+        .find("a.title")
+        .text(),
+      link: $(ad)
+        .find("a.title")
+        .attr("href"),
+      id: $(ad).attr('data-listing-id')
+    };
   });
 
-  return response;
+  return ads;
 };
--- KijijiScrape/index.js
+++ KijijiScrape/index.js
@@ -1,20 +1,20 @@
-const scraper = require('./components/scraper')
+const scraper = require("./components/scraper");
 
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = async function(context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    if (req.query.category) {
-        let body = await scraper(req)
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body,
-            isRaw: true
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+  if (req.query.category) {
+    let ads = await scraper(req);
+    body = ads.map(ad => ad.title.replace(/[\s\n\r]+/g, " ").trim());
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body,
+      isRaw: true
+    };
+  } else {
+    context.res = {
+      status: 400,
+      body: "Please pass a name on the query string or in the request body"
+    };
+  }
 };
